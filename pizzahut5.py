56a075ff6d934947df4b81ba56524576c529f7fd
novel175 <149669055+novel175@users.noreply.github.com>
2023-11-03 01:16:31 +0700

    Update pizzahut5.py

--- pizzahut5.py
+++ pizzahut5.py
@@ -56,3 +56,53 @@ daftar_ukuran = {
     2:"Regular",
     3:"Large"
 }
+# mengambil nama dari nomor yang diinputkan
+pilihan_menu = daftar_menu[menu]
+pilihan_crust = daftar_crust[crust]
+pilihan_ukuran = daftar_ukuran[ukuran]
+# Menentukan harga jika memilih crust pan
+if crust == 1:
+    # harga awal jika memilih crust pan
+    harga_pizza += 40909
+    # Menentukan harga berdasarkan ukuran pizza
+    if ukuran == 1:
+        harga_pizza += 0
+    elif ukuran == 2:
+        # jika pelanggan memilih ukuran regular
+        # harga pizza akan ditambah Rp.55.455
+        harga_pizza += 55455
+    else:
+        # jika pelanggan memilih ukuran Large
+        # harga pizza akan ditambah Rp.85.455
+        harga_pizza += 85455    
+# Menentukan harga jika pelanggan memilih crust stuffedcrust cheese
+elif crust == 2:
+    # harga awal jika memilih crust stuffedcrust cheese
+    harga_pizza += 52727
+    # Menentukan harga berdasarkan ukuran pizza
+    if ukuran == 1:
+        harga_pizza += 0
+    elif ukuran == 2:
+        # jika pelanggan memilih ukuran regular
+        # harga pizza akan ditambah Rp.63.637
+        harga_pizza += 63637
+    else:
+        # jika pelanggan memilih ukuran Large
+        # harga pizza akan ditambah Rp.100.909
+        harga_pizza += 100909
+# menentukan harga jika pelanggan memilih crust stuffedcrust sausage        
+elif crust == 3:
+    # harga awal jika memilih crust stuffedcrust cheese
+    harga_pizza += 50000
+    # Menentukan harga berdasarkan ukuran pizza
+    if ukuran == 1:
+        harga_pizza += 0
+    elif ukuran == 2:
+        # jika pelanggan memilih ukuran regular
+        # harga pizza akan ditambah Rp.62.727
+        harga_pizza += 62727
+    else:
+        # jika pelanggan memilih ukuran Large
+        # harga pizza akan ditambah Rp.99.091
+        harga_pizza += 99091
+        
