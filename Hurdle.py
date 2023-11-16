0ac9d61fd590ceab4562eeb7c5d7b53d62b2fe2e
yaasirf <148680817+yaasirf@users.noreply.github.com>
2023-11-16 22:21:11 +0700

    Update Hurdle.py

--- Hurdle.py
+++ Hurdle.py
@@ -33,6 +33,12 @@ def jump():
   move()
   turn_right()
   move()
+  turn_right()
+  move()
+  turn_left()
+
+while(not at_goal()):
+    jump()
 
 #Hurdle 3
 def turn_right():
@@ -57,3 +63,29 @@ def forward():
 
 while not at_goal():
     forward()
+
+#Hurdle 4
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    turn_left()
+    while wall_on_right():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    while front_is_clear():
+        move()
+    turn_left()
+
+def forward():
+    if front_is_clear():
+        move()
+    else:
+        jump()
+
+while not at_goal():
+    forward()
