4b63a93a4ddfba0d0fb8981ab85fc319a73b63f0
Novelita Fatma Anjany <149669055+novel175@users.noreply.github.com>
2023-11-16 22:06:38 +0700

    Update Hurdle.py

--- Hurdle.py
+++ Hurdle.py
@@ -28,3 +28,32 @@ def turn_right():
   turn_left()
 
 def jump():
+  move()
+  turn_left()
+  move()
+  turn_right()
+  move()
+
+#Hurdle 3
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+def forward():
+    if front_is_clear():
+        move()
+    else:
+        jump()
+
+while not at_goal():
+    forward()
